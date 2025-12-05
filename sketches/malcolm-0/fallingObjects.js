00d997648a87b2979868f290a2f9673dfe24dc9b
Gerpybera <malcolmsemedo333@gmail.com>
2025-12-05 09:09:58 +0100

    improved 0 visual

--- sketches/malcolm-0/fallingObjects.js
+++ sketches/malcolm-0/fallingObjects.js
@@ -15,7 +15,7 @@ const svgOriginalSize = 500;
 
 // Count of bodies inside the outer SVG
 let bodiesInsideCount = 0;
-let targetBodiesCount = 300; // Default target
+let targetBodiesCount = 250; // Default target
 
 // Mouse collision
 let mouseX = 0;
